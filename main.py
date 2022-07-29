47fa12740f4548188343392ad2800cf4235d7e95
Mowell(Intern),Jesse <mowellj@oclc.org>
2022-07-29 10:56:34 -0400

    Added .gitigonre

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ import os
 
 os.system("clear")
 color = input("Enter HEX code of color to check without the #: ")
-os.system(f"curl https://api.color.pizza/v1/{color} -s | jq")
+os.system(f"curl https://api.color.pizza/v1/{color} -s | jq > color.json")
