20cc186a6b68076c87836d7982d7cea66528bd57
Jesse Mowell <djessemowell@gmail.com>
2022-09-11 23:05:45 -0400

    Added shutdown line

--- main.py
+++ main.py
@@ -3,3 +3,4 @@ from playsound import playsound
 
 os.system('start "" /MAX "cmd /K python countdown.py && exit()')
 playsound('./sound.mp3')
+os.system('rundll32.exe powrprof.dll, SetSuspendState Sleep')
